0c2b4175d40332170103819ad0b32a699f94a67e
Hao Xu <proverbsonly@gmail.com>
2020-07-04 22:46:43 -0700

    add code for day 2

--- web-server/README.md
+++ web-server/README.md
@@ -1,3 +1,4 @@
 # Reference
 
 1. day-1: https://geektutu.com/post/gee-day1.html
+2. day-2: https://geektutu.com/post/gee-day2.html
--- web-server/main.go
+++ web-server/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 
 	"proverbs.top/webserver"
@@ -10,14 +9,19 @@ import (
 func main() {
 	r := webserver.New()
 
-	r.GET("/", func(w http.ResponseWriter, req *http.Request) {
-		fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
+	r.GET("/", func(c *webserver.Context) {
+		c.HTML(http.StatusOK, "<h1>Hello, Proverbs!</h1>")
 	})
 
-	r.GET("/hello", func(w http.ResponseWriter, req *http.Request) {
-		for k, v := range req.Header {
-			fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
-		}
+	r.GET("/hello", func(c *webserver.Context) {
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+	})
+
+	r.POST("/login", func(c *webserver.Context) {
+		c.JSON(http.StatusOK, webserver.H{
+			"username": c.PostForm("username"),
+			"password": c.PostForm("password"),
+		})
 	})
 
 	r.Run(":9999")
